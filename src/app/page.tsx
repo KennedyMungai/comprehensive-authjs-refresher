65ddada84d5f817490e6a776fb902898dcb743b4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-19 14:55:03 +0300

    Added some markup and logic to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-	return <div>HomePage</div>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-br from-violet-500 via-purple-500 to-indigo-500">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          🔐 Auth
+        </h1>
+        <p className="text-lg text-white">A simple authentication service</p>
+        <div>
+          <Button variant={"secondary"} size="lg">
+            Sign in
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default HomePage;
