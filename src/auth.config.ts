1b93ac6031873346ef2a9d2748ab0938d56e7474
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-20 08:54:17 +0300

    Implemented the credentials login

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,12 +1,30 @@
 import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
-import { SigninType } from "@/lib/validation";
+import { SigninSchema, SigninType } from "@/lib/validation";
+import { findUserByEmail } from "@/lib/user-queries";
+import bcrypt from "bcryptjs";
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
   providers: [
     Credentials({
-      async authorize(credentials: SigninType) {},
+      async authorize(credentials) {
+        const validatedFields = SigninSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await findUserByEmail(email);
+
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
+        }
+
+        return null;
+      },
     }),
   ],
 } satisfies NextAuthConfig;
