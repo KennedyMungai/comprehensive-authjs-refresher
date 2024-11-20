6eb6dc4c1aa90780609b9585c50ef4d539729487
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-20 08:29:17 +0300

    Defined some routes of note

---
+++ src/routes.ts
@@ -0,0 +1,25 @@
+/**
+ * An array of routes that are accessible to unauthenticated users
+ * @type {string[]}
+ */
+export const publicRoutes = ["/"];
+
+/**
+ * An array of routes that are used to authenticate users
+ * These redirect loggedIn users to /settings
+ * @type {string[]}
+ */
+export const authRoutes = ["/auth/signin", "/auth/signup"];
+
+/**
+ * The prefix of all api routes
+ *
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default route to redirect to after login
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
