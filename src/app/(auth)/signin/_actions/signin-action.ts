f2eb8315281ebde7e4b69a38ee8cd46024298863
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-20 09:01:50 +0300

    Implemented the credentials login

--- src/app/(auth)/signin/_actions/signin-action.ts
+++ src/app/(auth)/signin/_actions/signin-action.ts
@@ -1,10 +1,16 @@
 "use server";
 
+import { signIn } from "@/auth";
 import { actionClient } from "@/lib/safe-action";
 import { SigninSchema } from "@/lib/validation";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const loginAction = actionClient
   .schema(SigninSchema)
   .action(async ({ parsedInput: { email, password } }) => {
-    console.log({ email, password });
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
   });
