b811cca449c6a8b4806e34608909c1267462ff13
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-20 08:19:35 +0300

    Added some config to the middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,20 @@
-export { auth as middleware } from "@/auth";
+import NextAuth, { Session } from "next-auth";
+import { NextRequest } from "next/server";
+import authConfig from "@/auth.config";
+
+type NextAuthRequest = NextRequest & { auth: Session | null };
+
+export const { auth } = NextAuth(authConfig);
+
+export default auth((request: NextAuthRequest) => {
+  const { auth, nextUrl } = request;
+
+  const isLoggedIn = !!auth;
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
