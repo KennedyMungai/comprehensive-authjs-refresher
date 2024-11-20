b398dcc44578a8e91acbfc78447f37118b99650d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-20 10:54:57 +0300

    Extensding the session

--- src/auth.ts
+++ src/auth.ts
@@ -1,14 +1,30 @@
 import authConfig from "@/auth.config";
 import { db } from "@/db";
+import { findUserById } from "@/lib/user-queries";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
     async jwt({ token }) {
-      console.log({ token });
+      if (!token.sub) return token;
+
+      const existingUser = await findUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
 
       return token;
+    },
+    async session({ token, session }) {
+      if (session.user && token.sub) session.user.id = token.sub;
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
     },
   },
   pages: { signIn: "/signin" },
