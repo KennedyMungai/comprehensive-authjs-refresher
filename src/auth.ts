1da88b0dd8b9b34380f1ee2cf8e7256d17804173
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-20 10:30:38 +0300

    Added the jwt callback

--- src/auth.ts
+++ src/auth.ts
@@ -4,7 +4,15 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-	adapter: DrizzleAdapter(db),
-	session: { strategy: "jwt" },
-	...authConfig,
+  callbacks: {
+    async jwt({ token }) {
+      console.log({ token });
+
+      return token;
+    },
+  },
+  pages: { signIn: "/signin" },
+  adapter: DrizzleAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
