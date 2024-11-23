23bf5b7ce86e2a063c2b1523c6351c9ccc33f6b9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-23 11:35:51 +0300

    Getting the verification token by email

--- src/lib/verification-token.ts
+++ src/lib/verification-token.ts
@@ -3,9 +3,9 @@ import { verificationTokens } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import "server-only";
 
-export const getVerificationToken = async (token: string) =>
+export const getVerificationTokenByEmail = async (email: string) =>
   await db
     .select()
     .from(verificationTokens)
-    .where(eq(verificationTokens.token, token))
+    .where(eq(verificationTokens.identifier, email))
     .then((res) => res[0]);
