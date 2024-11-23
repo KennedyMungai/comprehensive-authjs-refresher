9ed0219980e1fb1f23f0888c88ec25f1c7259c80
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-23 12:08:09 +0300

    Added some checks for the loginAction

--- src/app/(auth)/signin/_actions/signin-action.ts
+++ src/app/(auth)/signin/_actions/signin-action.ts
@@ -2,12 +2,20 @@
 
 import { signIn } from "@/auth";
 import { actionClient } from "@/lib/safe-action";
+import { findUserByEmail } from "@/lib/user-queries";
 import { SigninSchema } from "@/lib/validation";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const loginAction = actionClient
   .schema(SigninSchema)
   .action(async ({ parsedInput: { email, password } }) => {
+    const existingUser = await findUserByEmail(email);
+
+    if (!existingUser || !existingUser.email || !existingUser.password)
+      throw new Error("Invalid credentials");
+
+    if (!existingUser.emailVerified) throw new Error("Email is not verified");
+
     await signIn("credentials", {
       email,
       password,
