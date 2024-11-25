fe723e54696967e0f074a4bbaf2cc4bf10290a7e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-25 08:48:29 +0300

    Addd the verify email and password reset routes to the public routes array

--- src/routes.ts
+++ src/routes.ts
@@ -2,19 +2,18 @@
  * An array of routes that are accessible to unauthenticated users
  * @type {string[]}
  */
-export const publicRoutes = ["/"];
+export const publicRoutes = [
+  "/",
+  "/signup/verify-email",
+  "/signin/password-reset",
+];
 
 /**
  * An array of routes that are used to authenticate users
  * These redirect loggedIn users to /settings
  * @type {string[]}
  */
-export const authRoutes = [
-  "/signin",
-  "/signup",
-  "/error",
-  "/signup/verify-email",
-];
+export const authRoutes = ["/signin", "/signup", "/error", ,];
 
 /**
  * The prefix of all api authentication routes
