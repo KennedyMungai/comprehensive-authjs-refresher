cb3e0b04dbb8df84ec5a9268739cd582b4949a26
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-11-26 14:19:54 +0300

    Deleted a pre-existing token

--- src/app/(auth)/signin/password-reset/_actions/password_reset_action.ts
+++ src/app/(auth)/signin/password-reset/_actions/password_reset_action.ts
@@ -1,10 +1,14 @@
 "use server";
 
 import { signIn } from "@/auth";
+import { db } from "@/db";
+import { passwordResetToken } from "@/db/schema";
+import { getPasswordResetTokenByEmail } from "@/lib/password-reset-token-queries";
 import { actionClient } from "@/lib/safe-action";
 import { findUserByEmail } from "@/lib/user-queries";
 import { PasswordResetSchema } from "@/lib/validation";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { eq } from "drizzle-orm";
 
 export const passwordResetAction = actionClient
   .schema(PasswordResetSchema)
@@ -19,6 +23,14 @@ export const passwordResetAction = actionClient
       throw new Error("Invalid credentials");
 
     if (!existingUser.emailVerified) throw new Error("Email is not verified");
+
+    const existingToken = await getPasswordResetTokenByEmail(email);
+
+    if (existingToken) {
+      await db
+        .delete(passwordResetToken)
+        .where(eq(passwordResetToken.email, email));
+    }
 
     await signIn("credentials", {
       email,
